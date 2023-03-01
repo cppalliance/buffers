db8f35573c6a6f798c469eb274c9d9e96ead1464
alandefreitas <alandefreitas@gmail.com>
2023-03-01 15:38:18 -0300

    [fold] install typo

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -227,9 +227,10 @@ ctest --output-on-failure
 #cmake --build .
 #cmake --build . --target check
 #ctest --output-on-failure
+#cd ../../../../..
 
 # Install Library
-cd ../../../../.. && mkdir __build_cmake_install_test__ && cd __build_cmake_install_test__
+mkdir __build_cmake_install_test__ && cd __build_cmake_install_test__
 cmake -DBOOST_INCLUDE_LIBRARIES=$SELF -DCMAKE_INSTALL_PREFIX=~/.local -DBoost_VERBOSE=ON -DBoost_DEBUG=ON ..
 cmake --build . --target install
 
